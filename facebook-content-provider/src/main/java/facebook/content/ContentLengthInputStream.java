5e28417e632b583fda8b906915ce3f5c86b428d6
Andrew Chen <yongjhih@gmail.com>
2015-10-15 03:42:18 +0800

    Add license

--- facebook-content-provider/src/main/java/facebook/content/ContentLengthInputStream.java
+++ facebook-content-provider/src/main/java/facebook/content/ContentLengthInputStream.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright 2013 Sergey Tarasevich
+ * Copyright 2015 8tory, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
